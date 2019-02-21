1bd58636bdede1c1dccddf36a958f00dde5627a4
AmbreO <AmbreO@users.noreply.github.com>
2019-02-21 12:30:05 +1000

    Typo fixes

--- Parsing.c
+++ Parsing.c
@@ -11,13 +11,13 @@ static char buffer[2048];
 char* readline(char* prompt){
   fputs(prompt, stdout);
   fgets(buffer, 2048, stdin);
-  char* cpy  malloc(strlen(buffer)+1);
+  char* cpy = malloc(strlen(buffer)+1);
   strcpy(cpy, buffer);
-  cpy[strln(cpy)-1] = '\0';
+  cpy[strlen(cpy)-1] = '\0';
   return cpy;
 }
 
-void add_histroy(char* unused){}
+void add_history(char* unused){}
 
 #else
 #include <editline/readline.h>
